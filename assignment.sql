873c3911f22ad42de2bb4bc84b7fd9c67f6d3c07
aothymoon59 <aothymoon59@gmail.com>
2024-11-28 09:52:21 +0600

    :sparkles: DONE-Retrieve the names of employees using a limit of 2, starting from the 3rd employee.

--- assignment.sql
+++ assignment.sql
@@ -2,3 +2,7 @@
 -- !/ Query 1:
 -- !/ Retrieve all employees with a salary greater than 60000
 SELECT * FROM employees WHERE salary > 60000;
+
+-- !/ Query 2:
+-- !/ Retrieve the names of employees using a limit of 2, starting from the 3rd employee.
+SELECT employee_name FROM employees LIMIT 2 OFFSET 2;
