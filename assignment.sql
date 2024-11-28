3c7e8ae7669a1e307c28a4dbe066d9e2959a1477
aothymoon59 <aothymoon59@gmail.com>
2024-11-28 10:27:57 +0600

    :sparkles: DONE-Retrieve the department name and the average salary of employees in each department

--- assignment.sql
+++ assignment.sql
@@ -38,3 +38,12 @@ WHERE
         SELECT MAX(salary)
         FROM employees
     );
+
+-- !/ Query 7:
+-- !/ Retrieve the department name and the average salary of employees in each department:
+
+SELECT department_name, avg(salary)
+from employees
+    JOIN departments USING (department_id)
+GROUP BY
+    department_name;
