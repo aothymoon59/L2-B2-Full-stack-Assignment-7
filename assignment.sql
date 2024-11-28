8c91c34c02c6ecb600a32d927dee1c4092eeca9f
aothymoon59 <aothymoon59@gmail.com>
2024-11-28 10:04:47 +0600

    :sparkles: DONE-Calculate and display the average age of all employees.

--- assignment.sql
+++ assignment.sql
@@ -6,3 +6,7 @@ SELECT * FROM employees WHERE salary > 60000;
 -- !/ Query 2:
 -- !/ Retrieve the names of employees using a limit of 2, starting from the 3rd employee.
 SELECT employee_name FROM employees LIMIT 2 OFFSET 2;
+
+-- !/ Query 3:
+-- !/ Calculate and display the average age of all employees. sample: 31.6
+SELECT ROUND(AVG(age), 1) AS average_age FROM employees;
